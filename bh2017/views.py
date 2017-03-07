face79287d1532f5eccbdd5e680046a37b443cae
felixo <alexei.eremin@gmail.com>
2017-03-07 16:51:29 +0300

    Pre 2

--- bh2017/views.py
+++ bh2017/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the BITVA HUDOJNIKOV 2017 index.")
+    return render(request, 'bh2017/index.html')
--- bitvahudojnikov/urls.py
+++ bitvahudojnikov/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^bh2017/', include('bh2017.urls')),
+    url(r'^$', include('bh2017.urls')),
     url(r'^admin/', admin.site.urls),
 ]
