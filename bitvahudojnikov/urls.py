7198ab957e1384837d5ab9f2f787fee0d19200c9
felixo <alexei.eremin@gmail.com>
2017-03-22 14:01:23 +0300

    adding jury

--- bitvahudojnikov/urls.py
+++ bitvahudojnikov/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('bh2017.urls', namespace='bh2017')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
